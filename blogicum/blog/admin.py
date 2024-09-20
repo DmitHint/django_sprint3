94f9bb15b5444ec404f57230de1169a67e0fe8dc
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-09-20 13:32:35 +0300

    Refactoring has been done according to the code review.

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,21 +1,16 @@
 from django.contrib import admin
-
 from .models import Post, Category, Location
 
-
+@admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text', 'pub_date',)
 
-
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description',)
 
-
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
-
 admin.site.empty_value_display = 'Не задано'
-admin.site.register(Post, PostAdmin)
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Location, LocationAdmin)
--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -4,13 +4,18 @@ from blog.models import Post, Category
 from django.conf import settings
 
 
-def index(request):
-    template = 'blog/index.html'
-    post_list = Post.objects.filter(
+def get_published_posts():
+    posts = Post.objects.filter(
         is_published=True,
         category__is_published=True,
         pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:settings.POSTS_PER_PAGE]
+    )
+    return posts
+
+
+def index(request):
+    template = 'blog/index.html'
+    post_list = get_published_posts()[:settings.POSTS_PER_PAGE]
 
     context = {'post_list': post_list}
     return render(request, template, context)
@@ -31,10 +36,12 @@ def category_posts(request, category_slug):
     template = 'blog/category.html'
     category = get_object_or_404(Category, is_published=True,
                                  slug=category_slug)
-    post_list = category.posts.filter(
+
+    posts = category.posts.filter(
         is_published=True,
         pub_date__lte=timezone.now()
     )
+
     context = {'category': category,
-               'post_list': post_list}
+               'posts': posts}
     return render(request, template, context)
