f107c4f098f0767cd84bc3136e3cd34e2dcb4594
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-11-08 23:51:19 +0300

    fix: flake8 errors

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,16 +1,20 @@
 from django.contrib import admin
 from .models import Post, Category, Location
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text', 'pub_date',)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description',)
 
+
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     search_fields = ('name',)
+
 
 admin.site.empty_value_display = 'Не задано'
